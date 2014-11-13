f0bc3e793f190f2a5fe6762e6b122df49bd5e5fe
John W. Mount <john@venuesoftware.com>
2014-11-12 18:08:58 -0800

    Set route to active_admin default which is root to: 'admin/dashboard#index'

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  # default root to go to the admin namespace, dashboard controller, index action:
+
+  root to: "admin/dashboard#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
