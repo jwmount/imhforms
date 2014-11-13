b2d2db97260a56b3442d235eeb71ae7f90f509bd
John W. Mount <john@venuesoftware.com>
2014-11-12 18:24:26 -0800

    Removed CanCan from Gemfile.  May be causing problems with Rails 4.x strong parameters.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,14 @@ class StudentsController < InheritedResources::Base
   private
 
     def student_params
-      params.require(:student).permit(:name, :birthdate, :grade, :teacher, :allergies, :diet, :sleep, :elimination, :concerns, :stability, :parent_histories, :household_members, :community_supports, :interaction_patterns, :well_attuned, :avoidant, :vigilant, :over_stimulating, :sensory_registration, :sensory_orientation, :sensory_interpretation, :response_organization, :response_execution, :adaptability, :sensory_threshold, :sensory_seeking_behaviors, :shares_attention, :regulation, :engagement, :interactions, :individuation, :mood, :solves_problems, :imagination, :creativity, :reasoning, :links_emotions, :perspective, :multiCausal_thinking, :selfReflective_thinking)
+      params.require(:student).permit(:name, :birthdate, :grade, :teacher, :allergies, :diet, 
+      	:sleep, :elimination, :concerns, :stability, :parent_histories, :household_members, 
+      	:community_supports, :interaction_patterns, :well_attuned, :avoidant, :vigilant, 
+      	:over_stimulating, :sensory_registration, :sensory_orientation, :sensory_interpretation, 
+      	:response_organization, :response_execution, :adaptability, :sensory_threshold, 
+      	:sensory_seeking_behaviors, :shares_attention, :regulation, :engagement, :interactions, 
+      	:individuation, :mood, :solves_problems, :imagination, :creativity, :reasoning, 
+      	:links_emotions, :perspective, :multiCausal_thinking, :selfReflective_thinking)
     end
 end
 
