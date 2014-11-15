b6956789f4dfd5f664ef75100b7bc9c068a3ddfd
John W. Mount <john@venuesoftware.com>
2014-11-15 14:41:17 -0800

    IMHFORMS -- remove unnecessary CRUD operations.

--- app/admin/observation.rb
+++ app/admin/observation.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Observation do
 
+  actions :all, :except => [:new, :edit, :show]
+  
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
