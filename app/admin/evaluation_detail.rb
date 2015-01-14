69c574e90e01c7012a264c877971674893ddfe5d
John W. Mount <john@venuesoftware.com>
2015-01-14 08:11:48 -0800

    Evaluation Detail date ranges working better.

--- app/admin/evaluation_detail.rb
+++ app/admin/evaluation_detail.rb
@@ -45,7 +45,7 @@
     # Date intervals (buckets), every 15 days beginning 1 Nov 2014
     # If @buckets exists just return it as will be same for given student.
     # TODO -- Harden this up, if dates are nil it will crash.
-    def date_buckets
+    def XXdate_buckets
       return @buckets unless @buckets.nil?
       min, max = Date.new()
       min = @student.developmental_levels.minimum("observed_on")
@@ -57,6 +57,34 @@
       @buckets
     end
     
+    # In this version @buckets is an array of ranges, i.e. @buckets[0] = range(first, last)
+    # Each month has two ranges, namely 1 - 15 and 16 - end.
+    # TODO -- last bucket can be/is one period in future, don't need to show this.
+    def date_buckets
+      min, max = Date.new()
+      @buckets = []
+      min = @student.developmental_levels.minimum("observed_on")
+      max = @student.developmental_levels.maximum("observed_on")
+
+      @buckets = [Range.new( Date.new(min.year, min.month, 1), Date.new(min.year, min.month, 15))]
+
+      # still have to get last day of first month to complete range for second half of first month
+      first_day = min + 1.month
+      last_day =  first_day - 1.day
+      @buckets << Range.new( Date.new(min.year, min.month, 16), Date.new(min.year, min.month, last_day.day ))
+
+      # now based on period.min we get the first element of @buckets which is itself a range
+      until @buckets.last.max > max do 
+        first_day = @buckets.last.max + 1.day
+        mid = Date.new(first_day.year, first_day.month, 15)
+        last_day =  first_day + 1.month - 1
+        @buckets << Range.new( first_day, mid )
+        @buckets << Range.new( mid + 1.day, last_day)
+      end
+      @buckets
+    end
+
+
     # for each recorder, return array of evaluations to match date_buckets.
     # if no value is recorded for a bucket, it's blank.
     def evaluations behavior, recorder, bucket
@@ -79,15 +107,15 @@
         
             tr 
               th h3 'Recorders'
-              date_buckets.each do |date|
-                th h4 b date
+              date_buckets.each do |bucket|
+                th h4 b bucket.min.strftime("%b #{bucket.min.day}-#{bucket.max.day}, %Y")
               end
 
             recorders.each do |name, count|
               tr
                 td h4 b name
                 @buckets.each do |b|
-                  td evaluations( behavior, name, [b, b+15] )
+                  td evaluations( behavior, name, [b.min, b.max] )
                 end
             end #recorders
           end
